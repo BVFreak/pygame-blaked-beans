7eb494bc97c218ce6f5c41ea7ac52252203fda27
BVFreak <blakev2009@gmail.com>
2022-09-22 18:44:57 +1000

    make everything

---
+++ level.py
@@ -0,0 +1,111 @@
+import pygame
+from particles import ParticleEffect
+from tiles import Tile
+from settings import tile_size
+from player import Player
+from particles import ParticleEffect
+
+class Level:
+    def __init__(self,level_data,surface):
+
+        # level setup
+        self.display_surface = surface
+        self.setup_level(level_data)
+        self.world_shift = 0
+        self.current_x = 0
+
+        # dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+
+    def create_jump_particles(self,pos):
+        jump_particle_sprite = ParticleEffect(pos,'jump')
+        self.dust_sprite .add(jump_particle_sprite)
+
+    def setup_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
+        for row_index,row in enumerate(layout):
+            for col_index,cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
+                if cell == 'X':
+                    tile = Tile((x,y),tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x,y),self.display_surface,self.create_jump_particles)
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player.x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player.x < 400 and direction_x < 0:
+            self.world_shift = 7
+            player.speed = 0
+
+        elif player.x > 800 and direction_x > 0:
+            self.world_shift = -7
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 7
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+            player.on_left = False
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+            player.on_right = False
+
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                    player.on_ground = True
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+                    player.on_ceiling = True
+
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
+
+    def run(self):
+
+        # level tiles
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+        self.scroll_x()
+
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+
+        # player
+        self.player.update()
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
+        self.player.draw(self.display_surface)
